546d8b4743bf79646091b10e5d173713e4e3e235
James <crowson.j@gmail.com>
2019-06-21 18:28:24 -0400

    Add .eslintrc file.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    }
+};
